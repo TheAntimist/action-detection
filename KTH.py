1914f6eb05974d6ec902c2a17d3b3f75c1cf791e
Ankush M <ankushmishra9@gmail.com>
2017-03-22 01:36:50 +0530

    Fixed the data representation and classification

--- KTH.py
+++ KTH.py
@@ -2,24 +2,30 @@ from dataset import base_dataset
 import numpy as np
 import os
 import subprocess as sp
-from sklearn.cluster import KMeans
+from sklearn.cluster import MiniBatchKMeans
+from sklearn.decomposition import PCA
 from collections import defaultdict
 from sklearn.externals import joblib
 
 
 class KTH(base_dataset):
+    labels = ["boxing", "handclapping", "handwaving", "jogging", "running", "walking"]
+    video_types = ["d1", "d2", "d3", "d4"]
 
-    def __init__(self, n_clusters=1500, path_to_videos=None, path_to_features=None, sequence_file=None):
-        self.path_to_videos = '/home/emanon/Desktop/BL/data/KTH/'
-        self.path_to_features = self.path_to_videos + "descr/"
+    train = [11, 12, 13, 14, 15, 16, 17, 18]
+    validation = [19, 20, 21, 23, 24, 25, 1, 4]
+    test = [22, 2, 3, 5, 6, 7, 8, 9, 10]
+
+    path_to_videos = '/home/emanon/Desktop/BL/data/KTH/'
+    path_to_features = path_to_videos + "descr/"
+
+    def __init__(self, n_clusters=1500, path_to_videos=None, path_to_features=None, sequence_file=None,
+                 init_file=None, verbose=False, calc_features=False):
+
         self.n_clusters = n_clusters
+        self.verbose = verbose
 
-        self.train = [11, 12, 13, 14, 15, 16, 17, 18]
-        self.validation = [19, 20, 21, 23, 24, 25, 1, 4]
-        self.test = [22, 2, 3, 5, 6, 7, 8, 9, 10]
-
-        self.labels = ["boxing", "handclapping", "handwaving", "jogging", "running", "walking"]
-        self.video_types = ["d1", "d2", "d3", "d4"]
+        self.X_tr = self.y_tr = self.X_val = self.y_val = self.X_test = self.y_test = None
 
         if path_to_videos:
             self.path_to_videos = path_to_videos
@@ -28,25 +34,34 @@ class KTH(base_dataset):
         if path_to_features:
             self.path_to_features = path_to_features
 
-        if sequence_file:
-            self.seq_dict = self.fill_sequence_dict(sequence_file)
-        else:
-            self.seq_dict = self.fill_sequence_dict()
+        if calc_features:
+            self.log("Calculating STIP Features on the dataset.")
+            if not os.path.exists(self.path_to_features): os.mkdir(self.path_to_features)
 
-        if not os.path.exists(self.path_to_features):
-            print("Calculating STIP Features on the dataset.")
-            os.mkdir(self.path_to_features)
             for file in os.listdir(path_to_videos):
                 filename, ext = os.path.splitext(file)
                 for sequence in self.seq_dict[filename]:
                     start, end = sequence.split('-')
-                    print("Calculating features for {} at sequence {}-{}".format(filename, start, end))
-                    self.calc_stip_features(path_to_videos, path_to_features + filename + "." + start + "-" + end + ".txt",
-                                     start, end, filename)
+                    self.log("Calculating features for {} at sequence {}-{}".format(filename, start, end))
+                    self.calc_stip_features(path_to_videos,
+                                            path_to_features + filename + "." + start + "-" + end + ".txt",
+                                            start, end, filename)
+
+        if os.path.exists(init_file) and init_file:
+            self.read_from_file(init_file)
+        else:
+            self.seq_dict = self.fill_sequence_dict()
+            # KMeans
+            self.kmeans = None
+            self.get_train_vocabulary(self.train + self.validation + self.test, self.path_to_features)
+
+    def log(self, message):
+        if self.verbose:
+            print(message)
 
     def fill_sequence_dict(self, file="sequences.txt"):
         '''
-        Creates a dictionary of files as key and the frame sequences as values of data type list.
+        Creates a dictionary of files, file as key and the frame sequences as values of a list.
         :return:
         '''
         d = defaultdict(list)
@@ -57,7 +72,11 @@ class KTH(base_dataset):
                 d[cols[0]].extend(frame_sequences)
         return d
 
-    def getn_seq(self, video_number):
+    def get_seq_count(self, video_number):
+        """
+        :param video_number:
+        :return: Returns the count of sequences present in the video_number.
+        """
         count = 0
         for number in video_number:
             for label in self.labels:
@@ -86,10 +105,6 @@ class KTH(base_dataset):
         # contains links to export the LD_LIBRARY_PATH correctly
 
         with open(path_to_videos + "video_list.txt", "w") as video_list:
-            # if video_files is None:
-            #     video_files = os.listdir(path_to_videos)
-            #
-            # for file in video_files:
             video_list.write(os.path.splitext(file)[0] + " " + start_frame + " " + end_frame + "\n")
 
         args = path_to_stipdet + " -i " + \
@@ -103,9 +118,9 @@ class KTH(base_dataset):
         try:
             os.remove(path_to_videos + "video_list.txt")
         except:
-            print("Error removing video_list.txt\n")
+            self.log("Error removing video_list.txt\n")
 
-    def getstipfeaturesfromfile(self, file):
+    def get_stip_features_from_file(self, file):
         '''
 
         Assumes the file contains the features of only one single video.
@@ -119,12 +134,19 @@ class KTH(base_dataset):
             position: Position of the Spatio Temporal Point on the the frames
             descriptors: HOG/HOF descriptors
         '''
-        position = np.genfromtxt(file, comments='#', usecols=(4, 5, 6, 7, 8))
-        descriptors = np.genfromtxt(file, comments='#')[:, 9:]
+        position = np.genfromtxt(file, comments='#', usecols=(4, 5, 6, 7, 8), dtype=np.int32)
+        descriptors = np.genfromtxt(file, comments='#')
+
+        x, *y = descriptors.shape
+
+        if not y:
+            descriptors = np.reshape(descriptors, (1, x))
+
+        descriptors = descriptors[:, 9:]
 
         return position, descriptors
 
-    def getboffromdescriptors(self, descriptors, num_centroids=1500):
+    def get_BOW_from_descriptors(self, descriptors, num_centroids=1500):
         '''
 
         :param descriptors: n x 160 matrix representing the descriptors for all points in all the videos.
@@ -132,29 +154,28 @@ class KTH(base_dataset):
                   this can then be used as a Feature vector for each video
         '''
 
-        n_iterations = 100
-        kmeans = KMeans(n_clusters=num_centroids, n_jobs=3, max_iter=n_iterations)
+        assert self.kmeans is not None, "Vocabulary hasn't been trained over the points"
 
-        result_cluster = kmeans.fit_predict(descriptors)
+        result_cluster = self.kmeans.predict(descriptors)
 
         histogram = np.zeros(num_centroids, dtype=np.int32)
         for cluster in result_cluster:
             cluster = int(cluster)
-            histogram[cluster] += + 1
+            histogram[cluster] += 1
         return histogram
 
-    def getfeaturevectorspersequence(self, n, video_number, path_to_features):
+    def get_BOW_from_file(self, n, video_number, path_to_features):
         '''
+        Retrieves the BOW for all the
 
         :param n: Number of videos to process, predefined to init the numpy matrices
-        :param n_clusters: number of clusters for bag of features
         :param video_number: list containing the video number
         :param path_to_features: path to stip features folder.
         :return: X, the feature vector along with y, the array of labels
         '''
 
         y = np.empty((n,), dtype=np.int32)  # one video is missing
-        X = np.empty((n, 72 + 90), dtype=np.int32)
+        X = np.empty((n, self.n_clusters), dtype=np.int32)
         i = 0
 
         for number in video_number:
@@ -167,101 +188,115 @@ class KTH(base_dataset):
                     for sequence in self.seq_dict[seq_file]:
                         start, end = sequence.split('-')
                         file = seq_file + "." + start + "-" + end + '.txt'
-                        print("Calculating Descriptor vector for file {}".format(file))
-                        position, descr = self.getstipfeaturesfromfile(path_to_features + file)
+                        # print("Calculating Descriptor vector for file {}".format(file))
+                        position, descr = self.get_stip_features_from_file(path_to_features + file)
 
-                        X[i] = descr
+                        X[i] = self.get_BOW_from_descriptors(descr, self.n_clusters)
                         y[i] = self.labels.index(label)
                         i += 1
 
         return X, y
 
-    def getVocabulary(self, n, video_number, path_to_features, n_clusters=1500):
+    def get_train_vocabulary(self, video_number, path_to_features, n_clusters=1500):
         '''
+        Clusters all the spatio-temporal points and produces a vocabulary of words.
+        This is then used to get a Bag of Words, for each sequence.
 
-        :param n: Number of videos to process, predefined to init the numpy matrices
         :param n_clusters: number of clusters for bag of features
-        :param video_number: list containing the video number
+        :param video_number: list containing the video number, almost always, this is all the videos.
         :param path_to_features: path to stip features folder.
-        :return: X, the histogram of the Clusters vector along with y, the array of labels
         '''
 
         DESCRIPTOR_BINS = 72 + 90
-        finalX = np.empty((len(self.labels), n_clusters), dtype=np.int32)
-        finalY = np.empty((len(self.labels),), dtype=np.int32) # one video is missing
-        i = 0
-        finali = 0
 
-        for label in self.labels:
+        self.log("Creating a Vocabulary for the dataset.")
+        if os.path.exists("descr.pkl"):
+            descriptors = joblib.load("descr.pkl")
+        else:
+            descriptors = np.empty((0, DESCRIPTOR_BINS), dtype=np.int32)
+            for label in self.labels:
+                for number in video_number:
+                    for type in self.video_types:
+                        if number is 13 and type is 'd3' and label is 'handclapping':  # ignore missing file.
+                            continue
 
-            tempX = np.empty((0, DESCRIPTOR_BINS), dtype=np.int32)
+                        seq_file = 'person' + str(number).zfill(2) + '_' + label + '_' + type
 
-            for number in video_number:
-                for type in self.video_types:
-                    if number is 13 and type is 'd3' and label is 'handclapping':  # ignore missing file.
-                        continue
+                        for sequence in self.seq_dict[seq_file]:
+                            start, end = sequence.split('-')
+                            file = seq_file + "." + start + "-" + end + '.txt'
 
-                    seq_file = 'person' + str(number).zfill(2) + '_' + label + '_' + type
+                            position, descr = self.get_stip_features_from_file(path_to_features + file)
 
-                    for sequence in self.seq_dict[seq_file]:
-                        start, end = sequence.split('-')
-                        file = seq_file + "." + start + "-" + end + '.txt'
-                        position, descr = self.getstipfeaturesfromfile(path_to_features + file)
+                            descriptors = np.concatenate((descriptors, descr), axis=0)
 
-                        tempX = np.concatenate((tempX, descr), axis=0)
+            joblib.dump(descriptors, "descr.pkl")
 
-            feature_hist = self.getboffromdescriptors(tempX, n_clusters)
-            finalX[finali] = feature_hist
-            finalY[finali] = self.labels.index(label)
-
-            finali += 1
-            print("Feature vector for {}: {}".format(label, feature_hist))
-
-        return finalX, finalY
+        self.log("Running KMeans on the points.")
+        # KMeans to cluster the descriptors
+        n_iterations = 100
+        self.kmeans = MiniBatchKMeans(n_clusters=n_clusters, verbose=self.verbose,
+                                      max_iter=n_iterations, batch_size=1000, n_init=8)
+        self.kmeans.fit(descriptors)
 
     def train_features_labels(self, from_file=None):
+        if self.X_tr is not None and self.y_tr is not None:
+            return self.X_tr, self.y_tr
+
         if from_file:
             X, y = joblib.load(from_file)
         else:
-            n_seq = self.getn_seq(self.train)
+            n_seq = self.get_seq_count(self.train)
 
-            X, y = self.getVocabulary(n_seq, self.train, self.path_to_features, self.n_clusters)
+            X, y = self.get_BOW_from_file(n_seq, self.train, self.path_to_features)
+
+        self.X_tr = X
+        self.y_tr = y
 
         return X, y
 
 
     def validation_features_labels(self, from_file=None):
+        if self.X_val is not None and self.y_val is not None:
+            return self.X_val, self.y_val
 
         if from_file:
             X, y = joblib.load(from_file)
         else:
-            n_val = self.getn_seq(self.validation)
+            n_val = self.get_seq_count(self.validation)
 
-            X, y = self.getVocabulary(n_val, self.validation, self.path_to_features, self.n_clusters)
+            X, y = self.get_BOW_from_file(n_val, self.train, self.path_to_features)
+
+        self.X_val = X
+        self.y_val = y
 
         return X, y
 
 
     def test_features_labels(self, from_file=None):
+        if self.X_test is not None and self.y_test is not None:
+            return self.X_test, self.y_test
 
         if from_file:
             X, y = joblib.load(from_file)
         else:
-            n_test = self.getn_seq(self.test)
+            n_test = self.get_seq_count(self.test)
 
-            X, y = self.getVocabulary(n_test, self.test, self.path_to_features, self.n_clusters)
+            X, y = self.get_BOW_from_file(n_test, self.train, self.path_to_features)
+
+        self.X_test = X
+        self.y_test = y
 
         return X, y
-
 
     def write_to_file(self, file):
         X_tr, y_tr = self.train_features_labels()
         X_val, y_val = self.validation_features_labels()
         X_test, y_test = self.test_features_labels()
-        joblib.dump((X_tr, y_tr, X_val, y_val, X_test, y_test), file)
-        return X_tr, y_tr, X_val, y_val, X_test, y_test
-
+        joblib.dump((X_tr, y_tr, X_val, y_val, X_test, y_test, self.seq_dict, self.kmeans), file)
 
     def read_from_file(self, file):
-        X_tr, y_tr, X_val, y_val, X_test, y_test = joblib.load(file)
-        return X_tr, y_tr, X_val, y_val, X_test, y_test
+        self.X_tr, self.y_tr, self.X_val,\
+        self.y_val, self.X_test, self.y_test, self.seq_dict, self.kmeans  = joblib.load(file)
+
+        return self.X_tr, self.y_tr, self.X_val, self.y_val, self.X_test, self.y_test, self.seq_dict, self.kmeans
